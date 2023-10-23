7cd4c572a16a700cd716804caa89453df92f17b2
juliorcjcos <juliorcjcos@gmail.com>
2023-10-23 10:55:26 -0500

    prueba 23-10-2023

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import { Inter } from 'next/font/google'
 import { LoginPageContainer } from '@/container/LoginPageContainer'
 import LoginPage from './loginPage'
 
-
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
